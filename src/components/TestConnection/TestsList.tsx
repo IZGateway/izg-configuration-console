8d95e5ae53aa3f5390485cf9a821fdd92109bc05
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2023-08-22 09:05:05 -0400

    fix: IGDD 1028- fix jest unit test
    * feat: Add UI for edit connection
    
    * feat: Add updating values on submit functionality
    
    * feat: Add running QBP test on step 3 after updated values by user
    
    * fix: restore code lost during rebase
    
    * fix: bug fixes on rendering hooks
    
    * fix: Bug fix on next button disabled logic
    
    * feat: Add validation on fields
    
    * feat: Add UUID validation of the password requirement
    
    * chores: Fix change history bug and organize code
    
    * fix: Save correct updated password in table
    
    * fix: fix for empty new password fields and submit redirect only one successful submit
    
    * fix: Fix fetching updated data and linting errors
    
    * fix: Lint error
    
    * fix: Add logic to reuse connection test component for qbp test
    
    * test: Add unit test
    
    * feat: Remove current password and update submit screen accordingly
    
    * fix: fix for Change password logic and lint errors
    
    * lint: Fix for lint errors
    
    * test: fix jest tests
    
    * feat: Add UNDERSCORE utility library to handle form validations
    
    * wip: fixed until 3rd step
    
    * wip: fix for submit functionality
    
    * fix: edit page is loading without too many redraw error
    
    * fix: Fix for running tests after step 3
    
    * fix: Lint errors fix
    
    * fix: remove packages folder
    
    * fix: remove console
    
    * feat: Auditing for destination change history
    
    * Revert "feat: Auditing for destination change history"
    
    This reverts commit d4a1cabd18553121204c2b4dee6a518261c00d23.
    
    * fix: install dependencies
    
    * fix: jest config and test files fix
    
    * fix: jest config and test files fix
    
    * fix: fix verify test and some lint errors
    
    * fix: lint errors
    
    ---------
    
    Co-authored-by: Brian Harris <sendbrianemail@gmail.com>

--- src/components/TestConnection/TestsList.tsx
+++ src/components/TestConnection/TestsList.tsx
@@ -44,64 +44,62 @@ const TestsList = ({
   const progressPct = Number(((passeddata / totaldata) * 100).toFixed())
 
   const list = () => (
-    <>
-      <List>
-        {testResults.map((item) => (
-          <>
-            <ListItem key={item.name} id={item.name}>
-              <ListItemIcon>
-                {item.status === 'PASS' && <CheckCircleIcon color="primary" />}
-                {item.status === 'FAIL' && <ErrorIcon color="secondary" />}
-                {item.status === 'WARNING' && (
-                  <ReportProblemIcon color="warning" />
-                )}
-                {item.status === 'SKIPPED' && (
-                  <ErrorOutlineIcon sx={{ color: '#424242' }} />
-                )}
-              </ListItemIcon>
-
-              {item.status === 'PASS' ? (
-                <ListItemText primary={item.name} />
-              ) : item.status === 'SKIPPED' ? (
-                <ListItemText
-                  primary={item.name}
-                  secondary={
-                    <Typography variant="body2" color="default">
-                      Cannot test Further on failure
-                    </Typography>
-                  }
-                />
-              ) : (
-                <ListItemText
-                  primary={item.name}
-                  secondary={
-                    <Typography variant="body2" color="secondary">
-                      {item.message}
-                    </Typography>
-                  }
-                />
+    <List>
+      {testResults.map((item) => (
+        <React.Fragment key={item.name}>
+          <ListItem id={item.name}>
+            <ListItemIcon>
+              {item.status === 'PASS' && <CheckCircleIcon color="primary" />}
+              {item.status === 'FAIL' && <ErrorIcon color="secondary" />}
+              {item.status === 'WARNING' && (
+                <ReportProblemIcon color="warning" />
               )}
-              <Chip
-                label={item.status === 'SKIPPED' ? 'N/A' : item.status}
-                variant="outlined"
-                color={
-                  item.status === 'PASS'
-                    ? 'primary'
-                    : item.status === 'SKIPPED'
-                    ? 'default'
-                    : 'secondary'
+              {item.status === 'SKIPPED' && (
+                <ErrorOutlineIcon sx={{ color: '#424242' }} />
+              )}
+            </ListItemIcon>
+
+            {item.status === 'PASS' ? (
+              <ListItemText primary={item.name} />
+            ) : item.status === 'SKIPPED' ? (
+              <ListItemText
+                primary={item.name}
+                secondary={
+                  <Typography variant="body2" color="default">
+                    Cannot test further on failure
+                  </Typography>
                 }
-                sx={{
-                  borderRadius: '4px',
-                  marginTop: '8px',
-                }}
               />
-            </ListItem>
-            <Divider />
-          </>
-        ))}
-      </List>
-    </>
+            ) : (
+              <ListItemText
+                primary={item.name}
+                secondary={
+                  <Typography variant="body2" color="secondary">
+                    {item.message}
+                  </Typography>
+                }
+              />
+            )}
+            <Chip
+              label={item.status === 'SKIPPED' ? 'N/A' : item.status}
+              variant="outlined"
+              color={
+                item.status === 'PASS'
+                  ? 'primary'
+                  : item.status === 'SKIPPED'
+                  ? 'default'
+                  : 'secondary'
+              }
+              sx={{
+                borderRadius: '4px',
+                marginTop: '8px',
+              }}
+            />
+          </ListItem>
+          <Divider />
+        </React.Fragment>
+      ))}
+    </List>
   )
 
   const buttonGroup = () => (
