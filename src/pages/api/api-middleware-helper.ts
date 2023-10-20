5a472d0464fed1485e3c8a0872134ad373fa6ffc
akanuri9 <118755941+akanuri9@users.noreply.github.com>
2023-10-20 11:32:49 -0400

    fix: session token in middleware
    Igdd-1068 log connectivity test
    
    * fix: update healthcheck log entry as per feedback
    
    * feat: add logging for connection test and minor changes
    
    * fix: ci-cd deploy to stage
    
    * fix: session token in middleware

--- src/pages/api/api-middleware-helper.ts
+++ src/pages/api/api-middleware-helper.ts
@@ -18,12 +18,18 @@ const captureErrors: Middleware = async (req, res, next) => {
 
 // log the api requests and response code
 const logRequest: Middleware = async (req, res, next) => {
-  const sessionToken = req.cookies['next-auth.session-token']
-  const decoded = await decode({
+  const sessionToken =
+    req.cookies['next-auth.session-token'] ||
+    req.cookies['__Secure-next-auth.session-token']
+  const session = await decode({
     token: sessionToken,
     secret: process.env.NEXTAUTH_SECRET,
   })
-  logger.info('Api request ' + req.url, { req, res, user: decoded.email })
+  logger.debug('Api request ' + req.url, {
+    req,
+    res,
+    user: session.email || null,
+  })
   await next()
 }
 
@@ -33,7 +39,7 @@ const checkAccessToDestId: Middleware = async (req, res, next) => {
   const session = await getServerSession(req, res, authOptions)
   const hasAccess = hasAccessToDestId(destId, session)
   if (hasAccess) {
-    logger.info('Api request ' + req.url, {
+    logger.debug('Api request ' + req.url, {
       req,
       res,
       user: session.user.email,
@@ -41,7 +47,7 @@ const checkAccessToDestId: Middleware = async (req, res, next) => {
     await next()
   } else {
     res.status(401).send('unauthorized')
-    logger.info('Api request ' + req.url, {
+    logger.debug('Api request ' + req.url, {
       req,
       res,
       user: session.user.email,
@@ -54,7 +60,7 @@ const checkAccessToDestIdSlug: Middleware = async (req, res, next) => {
   const session = await getServerSession(req, res, authOptions)
   const hasAccess = hasAccessToDestId(destId, session)
   if (hasAccess) {
-    logger.info('Api request ' + req.url, {
+    logger.debug('Api request ' + req.url, {
       req,
       res,
       user: session.user.email,
@@ -62,7 +68,7 @@ const checkAccessToDestIdSlug: Middleware = async (req, res, next) => {
     await next()
   } else {
     res.status(401).send('unauthorized')
-    logger.info('Api request ' + req.url, {
+    logger.debug('Api request ' + req.url, {
       req,
       res,
       user: session.user.email,
