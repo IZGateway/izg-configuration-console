416e61e32680fe925e02349233b5d080fad664f1
Brian Harris <sendbrianemail@gmail.com>
2023-11-09 09:51:40 -0500

    fix: logout destroys local and idp sessions

--- src/components/ConnectionTable/index.tsx
+++ src/components/ConnectionTable/index.tsx
@@ -167,7 +167,7 @@ const ConnectionsTable = () => {
                       </Box>
                       <Box sx={{ fontWeight: 'bold' }}>Retry Strategy:</Box>
                       <Box sx={{ fontWeight: 'regular' }}>
-                        {params.row.retry_strategy}
+                        {params.row.retryStrategy}
                       </Box>
                     </CardContent>
                   )}
