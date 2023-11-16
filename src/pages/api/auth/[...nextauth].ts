30710c8696129c8f6f60763fe5a5f1c33e52ec5c
Brian <sendbrianemail@gmail.com>
2023-11-16 15:45:40 -0500

    fix: env variables can be configured for use in the browser (#73)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import OktaProvider from 'next-auth/providers/okta'
 import logger from '../../../../logger'
 import _ from 'lodash'
 
-const userInfoEndpoint = `${process.env.OKTA_ISSUER}/oauth2/v1/userinfo`
+const userInfoEndpoint = `${process.env.NEXT_PUBLIC_OKTA_ISSUER}/oauth2/v1/userinfo`
 const isDebugging =
   (`${process.env.NEXTAUTH_DEBUG}` as unknown as boolean) || false
 export const authOptions = {
@@ -13,7 +13,7 @@ export const authOptions = {
     OktaProvider({
       clientId: process.env.OKTA_CLIENT_ID,
       clientSecret: process.env.OKTA_CLIENT_SECRET,
-      issuer: process.env.OKTA_ISSUER,
+      issuer: process.env.NEXT_PUBLIC_OKTA_ISSUER,
       idToken: true,
       authorization: { params: { scope: 'openid email profile groups' } },
     }),
@@ -27,6 +27,7 @@ export const authOptions = {
       if (token) {
         session.user.id = token.id
         session.accessToken = token.accessToken
+        session.user.groups = token.groups
         session.user.isAdmin = token?.groups?.includes(
           process.env.OPERATIONS_GROUP
         )
