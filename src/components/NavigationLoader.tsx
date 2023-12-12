7db2ff85333565bcff2a33f29a0d17fb08e3b04a
Brian Harris <sendbrianemail@gmail.com>
2023-12-12 11:31:53 -0500

    feat: navigation spinner displays between route changes

---
+++ src/components/NavigationLoader.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import { Box, CircularProgress } from '@mui/material'
+import izgLogo from '../public/IZ_Gateway_Logo 1.png'
+import Image from 'next/image'
+
+const LOADER_THRESHOLD = 250
+
+export default function NavigationLoader(props) {
+  const [isLoading, setLoading] = React.useState(false)
+  const router = useRouter()
+
+  React.useEffect(() => {
+    let timer
+
+    const start = () =>
+      (timer = setTimeout(() => setLoading(true), LOADER_THRESHOLD))
+
+    const end = () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+      setLoading(false)
+    }
+
+    router.events.on('routeChangeStart', start)
+    router.events.on('routeChangeComplete', end)
+    router.events.on('routeChangeError', end)
+
+    return () => {
+      router.events.off('routeChangeStart', start)
+      router.events.off('routeChangeComplete', end)
+      router.events.off('routeChangeError', end)
+
+      if (timer) {
+        clearTimeout(timer.current)
+      }
+    }
+  }, [router.events])
+
+  if (!isLoading) return null
+
+  return (
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#0006',
+        color: '#fff',
+        zIndex: 999,
+      }}
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box sx={{ m: 1, position: 'relative' }}>
+          <Image src={izgLogo} alt="izg logo" width={70} />
+          <CircularProgress
+            size={80}
+            sx={{
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              zIndex: 1,
+            }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
