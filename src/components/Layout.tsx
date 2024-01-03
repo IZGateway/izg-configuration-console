fbe79b0f2441b50eabb7ef1e45a190ba7517526d
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-01-03 11:51:54 -0500

    fix: linting

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,41 +1,40 @@
-/** @jsxImportSource @emotion/react */
-import Box from '@mui/material/Box'
-import Navigation from './Navigation'
-
+import React from 'react';
+import Box from '@mui/material/Box';
+import Navigation from './Navigation';
 const container = {
   display: 'flex',
   flexDirection: 'column',
   minHeight: '100vh',
   boxShadow: 'inset 0px 0px 25px 10px rgba(0, 0, 0, 0.25)',
-}
+};
 
 const content = {
   display: 'flex',
   flex: 1,
-}
+};
 
 const pageContainer = {
   padding: 3,
   width: '80vw',
   flexGrow: 1,
-  paddingTop: '7em',
-}
+  paddingTop: '4em',
+};
 
 interface DashboardLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const Layout = ({ children }: DashboardLayoutProps) => {
+const Layout = ({ children, }: DashboardLayoutProps) => {
   return (
     <Box sx={container}>
       <Box sx={content}>
-        <Navigation />
+        <Navigation/>
         <Box role="main" sx={pageContainer}>
           {children}
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
