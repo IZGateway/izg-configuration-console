56fd22450631d3ab4d1d8ed04df8924df61aa5e9
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-01-08 13:04:44 -0500

    ui: add palette to have global color variables

---
+++ src/styles/theme/palette.ts
@@ -0,0 +1,27 @@
+const palette = {
+    active: '#66926d',
+    background: '#F9F9F9',
+    black: '#000',
+    divider: '#c2c6ca',
+    error: '#B50E16',
+    errorDark: '#6D191D',
+    grey: '#A7A7A7',
+    greyLight: '#efefef',
+    greyMain: '#afafaf',
+    greyDarkTypography: '#212121',
+    greyDark: '#101010',
+    primaryLight: '#00D998',
+    primary: '#015A2F',
+    primaryDark: '#1E4D3B',
+    secondaryLight: '#FACBCE',
+    secondary: '#8D3359',
+    secondaryDark: '#6B4559',
+    warningDark: '#5C3607',
+    warning: '#83380E',
+    warningLight: '#F2D0A7',
+    white: '#FFFFFF',
+    border: '#BFBFBF',
+  };
+  
+  export default palette;
+  
