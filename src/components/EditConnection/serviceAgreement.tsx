84a8a4b830f1355fc2b31a9959aa53b86f78bbff
Matthew Stankiewicz <stankm@pointclickcare.com>
2024-02-02 14:12:05 -0500

    fix: take out margin causing container not to be aligned

--- src/components/EditConnection/serviceAgreement.tsx
+++ src/components/EditConnection/serviceAgreement.tsx
@@ -28,35 +28,48 @@ const ServiceAgreement = (props: PolicyProps) => {
       />
       <Divider />
       <CardContent>
-        <Typography variant="body1" component="div">
+        <Typography gutterBottom variant="body2" component="body">
           I hereby attest that I have been duly authorized by the jurisdiction
           to make changes to the Immunization (IZ) Gateway configuration for my
           jurisdiction. I understand the responsibility entrusted to me and
           acknowledge that any alterations made shall comply with all applicable
           laws, regulations, and policies governing system configurations within
-          the jurisdiction. I affirm that I have undergone the necessary
-          training and possess the technical competence required to carry out
-          system configuration changes effectively and responsibly. I am aware
-          of the potential risks associated with such modifications and commit
-          to implementing appropriate safeguards to ensure the integrity,
-          availability, and confidentiality of the system and its data.
+          the jurisdiction.
+        </Typography>
+        <Typography gutterBottom variant="body2" component="body">
+          I affirm that I have undergone the necessary training and possess the
+          technical competence required to carry out system configuration
+          changes effectively and responsibly. I am aware of the potential risks
+          associated with such modifications and commit to implementing
+          appropriate safeguards to ensure the integrity, availability, and
+          confidentiality of the system and its data.
+        </Typography>
+        <Typography gutterBottom variant="body2" component="body">
           Furthermore, I understand that my authorization is subject to any
           limitations or conditions imposed by the jurisdiction, and I shall
           adhere to all established procedures and protocols governing IZ
           Gateway configuration changes for my jurisdiction. I will exercise due
           diligence in evaluating the impact of any modifications and seek
-          necessary approvals before implementing them. I acknowledge that the
-          IZ Gateway may monitor and audit the changes made to the system
-          configuration as part of their oversight and compliance efforts. I
-          will cooperate fully with any such audits and provide any requested
-          information or documentation related to the changes made. I undertake
-          to prioritize the security and stability of the system during the
-          configuration process, taking into account the potential impact on
-          users, data, and overall exchange. By clicking Accept below, I declare
-          that the information attested is accurate to the best of my knowledge.
-          I understand that any misrepresentation or violation of the
-          jurisdiction’s or IZ Gateway&apos;s policies may result in the
-          revocation of my authorization and potential legal consequences.
+          necessary approvals before implementing them.
+        </Typography>
+        <Typography gutterBottom variant="body2" component="body">
+          I acknowledge that the IZ Gateway may monitor and audit the changes
+          made to the system configuration as part of their oversight and
+          compliance efforts. I will cooperate fully with any such audits and
+          provide any requested information or documentation related to the
+          changes made. I undertake to prioritize the security and stability of
+          the system during the configuration process, taking into account the
+          potential impact on users, data, and overall exchange.
+        </Typography>
+        <Divider sx={{ mt: 2, mb: 2 }} />
+        <Typography gutterBottom variant="caption" component="body">
+          <strong>
+            By clicking Accept below, I declare that the information attested is
+            accurate to the best of my knowledge. I understand that any
+            misrepresentation or violation of the jurisdiction’s or IZ
+            Gateway&apos;s policies may result in the revocation of my
+            authorization and potential legal consequences.
+          </strong>
         </Typography>
         <FormControl>
           <RadioGroup onChange={props.clickOnAgree}>
