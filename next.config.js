374061b2bd479abd8283e40f06363ab4b7526bd0
Brian Harris <sendbrianemail@gmail.com>
2024-05-07 08:53:23 -0400

    fix: removed redirects so homepage is now landing page

--- next.config.js
+++ next.config.js
@@ -13,15 +13,6 @@ module.exports = async (phase, { defaultConfig }) => {
       OPERATIONS_GROUP: `${process.env.OPERATIONS_GROUP}`,
       USER_GROUP: `${process.env.USER_GROUP}`,
     },
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
     webpack(config, { nextRuntime }) {
       // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
       if (typeof nextRuntime === 'undefined') {
