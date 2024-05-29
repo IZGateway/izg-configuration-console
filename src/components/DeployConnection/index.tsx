e0edbca356d71433371905ae3f5766cc19de1e62
Palak Patel <Papatel@ainq.com>
2024-05-29 13:17:59 -0400

    fix: Remove snackbar component from deploy page

--- src/components/DeployConnection/index.tsx
+++ src/components/DeployConnection/index.tsx
@@ -6,34 +6,9 @@ import ViewChangeRequestTicket from './viewChangeRequestTicket'
 import DeployConfirmation from './deployConfirmation'
 import DetailsChangeRequest from './detailsChangeRequest'
 import MakeChanges from './makeChanges'
-import CombinedContext from '../../contexts/app'
-import { useEffect, useState, useContext } from 'react'
-import _ from 'lodash'
-import CustomSnackbar from '../SnackBar'
 
 const JIRA_STATUS_FOR_DEPLOY = 'Approved'
 const DeployConnection = (props) => {
-  const { alert, setAlert } = useContext(CombinedContext)
-  const [showSnackbar, setShowSnackbar] = useState(false)
-
-  useEffect(() => {
-    if (!_.isEmpty(alert.level)) {
-      setShowSnackbar(true)
-    } else {
-      setShowSnackbar(false)
-    }
-  }, [alert])
-
-  const handleClose = () => {
-    setShowSnackbar(false)
-    setAlert({
-      level: '',
-      jurisdiction: '',
-      dest_type: '',
-      message: '',
-    })
-  }
-
   const {
     data: changerequestData,
     error: changerequestError,
@@ -112,12 +87,6 @@ const DeployConnection = (props) => {
             submittingValue={changerequestData}
           />
         </Box>
-        <CustomSnackbar
-          open={showSnackbar}
-          severity={alert.level}
-          message={alert.message}
-          onClose={handleClose}
-        />
       </Box>
     </>
   )
