33fa08f52bf80ffcc46ae411158012f0ab245897
Brian Harris <sendbrianemail@gmail.com>
2024-10-09 10:19:41 -0400

    fix: response echobacks that include the expected text are now a test pass

--- src/lib/connectiontests/impl/CONNECTIVITY.ts
+++ src/lib/connectiontests/impl/CONNECTIVITY.ts
@@ -218,7 +218,7 @@ function processBody(
             requestEchoback,
             responseEchoback
           ),
-          status: TestStatus.WARNING,
+          status: TestStatus.PASS,
         },
       ])
     } else if (
