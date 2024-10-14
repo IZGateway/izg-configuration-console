62926a4c87d9a43a0467e1aef08e4a35660f7e44
Brian Harris <sendbrianemail@gmail.com>
2024-10-14 10:22:04 -0400

    chore: bumped to version 1.2.0

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -307,7 +307,7 @@ function HomeComponent() {
               pb={1}
             >
               <Typography variant="caption">
-                Version 1.0.4-{process.env.NEXT_PUBLIC_BUILD_ID} | Immunization
+                Version 1.2.0-{process.env.NEXT_PUBLIC_BUILD_ID} | Immunization
                 (IZ) Gateway Configuration Console 2024
               </Typography>
               <Box
