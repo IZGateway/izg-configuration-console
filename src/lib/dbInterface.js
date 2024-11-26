e1d6311339e2518fae3adb2aa390089e7a08a28a
Keith W. Boone <kboone@ainq.com>
2024-11-26 11:52:14 -0500

    Fix imports in dbInterface.js

--- src/lib/dbInterface.js
+++ src/lib/dbInterface.js
@@ -1,29 +1,29 @@
 /**
  *	The dbInterface object represents the interface to the database.  
  */
-import { upsertDestinationChangeRequest, deleteDestinationChangeRequest } from './lib/queries/mutate/destinationchangerequest' 
-import deleteDraftValues from './lib/queries/mutate/deleteDraftValues'
-import cancelChangeRequest from './lib/queries/mutate/cancelChangeRequest'
-import updatedAuditedDestination from './lib/queries/mutate/destination'
-import upsertDraftRecord from './lib/queries/mutate/upsertDraftRecord'
-import maintenanceRequest from './lib/queries/mutate/maintenanceRequest'
-import updateChangeRequest from './lib/queries/mutate/updateChangeRequest'
+import { upsertDestinationChangeRequest, deleteDestinationChangeRequest } from './queries/mutate/destinationchangerequest' 
+import deleteDraftValues from './queries/mutate/deleteDraftValues'
+import cancelChangeRequest from './queries/mutate/cancelChangeRequest'
+import updatedAuditedDestination from './queries/mutate/destination'
+import upsertDraftRecord from './queries/mutate/upsertDraftRecord'
+import maintenanceRequest from './queries/mutate/maintenanceRequest'
+import updateChangeRequest from './queries/mutate/updateChangeRequest'
 
-import destination from './lib/queries/fetch/destination'
-import destinations from './lib/queries/fetch/destinations'
-import destinationaudithistory from './lib/queries/fetch/destinationaudithistory'
-import destinationchangerequest from './lib/queries/fetch/destinationchangerequest'
-import destinationType from './lib/queries/fetch/destinationType'
-import fetchDraftRecord from './lib/queries/fetch/draftrecord'
-import jurisdiction from './lib/queries/fetch/jurisdiction'
-import passwordComparison from './lib/queries/fetch/passwordComparison'
+import destination from './queries/fetch/destination'
+import destinations from './queries/fetch/destinations'
+import destinationaudithistory from './queries/fetch/destinationaudithistory'
+import destinationChangeRequest from './queries/fetch/destinationchangerequest'
+import destinationType from './queries/fetch/destinationType'
+import fetchDraftRecord from './queries/fetch/draftrecord'
+import jurisdiction from './queries/fetch/jurisdiction'
+import passwordComparison from './queries/fetch/passwordComparison'
 
 
 export const dbInterface = {
 	destination: destination,
 	destinations: destinations,
 	destinationaudithistory: destinationaudithistory,
-	destinationchangerequest: destinationchangerequest,
+	destinationChangeRequest: destinationChangeRequest,
 	destinationType : destinationType,
 	fetchDraftRecord: fetchDraftRecord,
 	jurisdiction: jurisdiction,
