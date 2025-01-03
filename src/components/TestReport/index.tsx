2f6313327d7250da8e66075de771a43516c54e39
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2025-01-03 15:11:13 -0500

    feat: test report can be generated for multiple connection status
    * feat: Test report
    
    * IGDD-1827 (#166)
    
    * ui: update manage connections to show custom table footer
    
    * Igdd 1827 UI (#167)
    
    * ui: update manage connections to show custom table footer
    
    * Revert "Igdd 1827 UI (#167)" (#168)
    
    This reverts commit fc7405bea47bfb9117fafde37bd559b428460519.
    
    * Revert "IGDD-1827 (#166)" (#169)
    
    This reverts commit cdf64a08e6775465e038253ecef255882acc9048.
    
    * ui: make limited change to footer (#170)
    
    * fix: lint error fix
    
    * IGDD-1827-UI Test Reports (#172)
    
    * ui: make limited change to footer
    
    * fix: removed comments
    
    * fix: remove cookie once out of test report page
    
    ---------
    
    Co-authored-by: mattystank <32516464+mattystank@users.noreply.github.com>

---
+++ src/components/TestReport/index.tsx
@@ -0,0 +1,280 @@
+import React from 'react'
+import {
+  DataGrid,
+  GridFooter,
+  GridSlots,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarQuickFilter,
+} from '@mui/x-data-grid'
+import WarningAmberIcon from '@mui/icons-material/WarningAmber'
+import ErrorIcon from '@mui/icons-material/Error'
+import { Box, Typography, Card, Chip, Button } from '@mui/material'
+import palette from '../../styles/theme/palette'
+import router from 'next/router'
+import Cookies from 'js-cookie'
+const dataGridCustom = {
+  '&.MuiDataGrid-root.MuiDataGrid-autoHeight.MuiDataGrid-root--densityComfortable':
+    {
+      marginTop: '-8px',
+      zIndex: 1,
+      paddingTop: '1em',
+      border: 'none',
+    },
+  '& .MuiDataGrid-main': {
+    marginTop: '-8px',
+    backgroundColor: palette.white,
+    borderRadius: '0 0 30px 30px',
+    border: `1px solid ${palette.border}`,
+    paddingBottom: '1em',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.25)',
+  },
+  '& .MuiDataGrid-row:hover': {
+    bgcolor: '#00000010',
+  },
+  '& .MuiFormControl-root.MuiTextField-root.css-3be3ve-MuiFormControl-root-MuiTextField-root-MuiDataGrid-toolbarQuickFilter':
+    {
+      width: '32vw',
+    },
+  '& .MuiDataGrid-columnHeaders': {
+    backgroundColor: palette.white,
+  },
+  '& .MuiDataGrid-toolbarContainer': {
+    backgroundColor: palette.white,
+    padding: '24px 16px 16px 16px',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.25)',
+    border: `1px solid ${palette.border}`,
+    marginBottom: '8px',
+  },
+  '& svg.MuiSvgIcon-root.MuiSvgIcon-fontSizeSmall.MuiDataGrid-sortIcon.css-ptiqhd-MuiSvgIcon-root':
+    {
+      color: palette.primary,
+    },
+  '& .MuiDataGrid-footerContainer.MuiDataGrid-footerContainer': {
+    width: 'auto',
+    borderRadius: '60px',
+    float: 'right',
+    margin: '2em 0',
+    justifyContent: 'center',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.25)',
+    backgroundColor: palette.white,
+  },
+  '& .MuiTablePagination-actions': {
+    color: palette.primary,
+  },
+  '& .MuiTablePagination-selectIcon.MuiSelect-icon.MuiSelect-iconStandard.css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon':
+    {
+      color: palette.primary,
+    },
+  '& .MuiDataGrid-virtualScroller': {
+    overflow: 'hidden',
+  },
+  '.highlight': {
+    bgcolor: palette.errorHighLight,
+  },
+}
+
+const renderStatus = (value) => {
+  let color
+  const label = value === 'SKIPPED' ? 'N/A' : value
+  let icon = null
+
+  if (value === 'PASS') {
+    color = 'primary'
+  } else if (value === 'WARNING') {
+    color = 'warning'
+    icon = <WarningAmberIcon fontSize="small" />
+  } else if (value === 'FAIL') {
+    color = 'error'
+    icon = <ErrorIcon fontSize="small" />
+  }
+
+  return (
+    <Chip
+      icon={icon}
+      label={label}
+      variant="outlined"
+      color={color}
+      sx={{
+        borderRadius: '4px',
+        marginTop: '8px',
+      }}
+    />
+  )
+}
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarQuickFilter />
+      <div style={{ marginLeft: 'auto' }}>
+        <GridToolbarExport
+          printOptions={{ disableToolbarButton: true }}
+          csvOptions={{
+            fields: [
+              'jurisdiction',
+              'destination',
+              'environment',
+              'dnsLookup',
+              'tcpConnectivity',
+              'tlsVersion',
+              'cipherSuites',
+              'connectivity',
+              'wsdl',
+              'hl7Query',
+            ],
+          }}
+        />
+      </div>
+    </GridToolbarContainer>
+  )
+}
+const handleGoBack = () => {
+  Cookies.remove('destination')
+  router.push('/manageconnections')
+}
+
+const CustomFooter = () => {
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Button
+        variant="outlined"
+        onClick={handleGoBack}
+        color="primary"
+        sx={{
+          borderRadius: '30px',
+        }}
+      >
+        Go Back to Connections
+      </Button>
+      <GridFooter />
+    </Box>
+  )
+}
+const TestReportTable = ({
+  connectionTestResults,
+  destinations,
+  destinationDetails,
+}) => {
+  console.log(destinationDetails)
+  const rows = destinationDetails.map((dest) => {
+    const result = connectionTestResults.find(
+      (testResult) =>
+        testResult.destId === dest.destId && testResult.destType === dest.type
+    )
+
+    return {
+      id: dest.destId + dest.destTypeId,
+      jurisdiction: dest?.jurisdiction || 'N/A',
+      destination: dest.destId,
+      environment: result?.destType || 'N/A',
+      dnsLookup: result?.dns || 'N/A',
+      tcpConnectivity: result?.tcp || 'N/A',
+      tlsVersion: result?.tls || 'N/A',
+      cipherSuites: result?.cipher || 'N/A',
+      wsdl: result?.wsdl || 'N/A',
+      connectivity: result?.connectivity || 'N/A',
+      hl7Query: result?.hl7 || 'N/A',
+    }
+  })
+  const columns = [
+    { field: 'jurisdiction', headerName: 'JURISDICTION', flex: 1 },
+    { field: 'destination', headerName: 'DESTINATION', flex: 1 },
+    { field: 'environment', headerName: 'ENVIRONMENT', flex: 1 },
+    {
+      field: 'dnsLookup',
+      headerName: 'DNS LOOKUP',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'tcpConnectivity',
+      headerName: 'TCP CONNECTIVITY',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'tlsVersion',
+      headerName: 'TLS VERSION',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'cipherSuites',
+      headerName: 'CIPHER SUITES',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'connectivity',
+      headerName: 'CONNETCIVITY',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'wsdl',
+      headerName: 'WSDL',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+    {
+      field: 'hl7Query',
+      headerName: 'HL7 QUERY',
+      flex: 1,
+      renderCell: (params) => renderStatus(params.value),
+    },
+  ]
+
+  return (
+    <div>
+      <Box>
+        <Card
+          sx={{
+            position: 'relative',
+            zIndex: 10,
+            height: 'auto',
+            boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.40)',
+            marginBottom: '-16px',
+          }}
+        >
+          <Typography
+            id="title-table"
+            sx={{ padding: 2, fontSize: '1.75rem', fontWeight: 700 }}
+            flexGrow={1}
+            display="flex"
+            align="center"
+          >
+            Result For Test Reports ({destinations.length})
+          </Typography>
+        </Card>
+      </Box>
+      <DataGrid
+        sx={dataGridCustom}
+        rows={rows}
+        columns={columns}
+        pageSizeOptions={[5, 25, 50, 100]}
+        autoHeight
+        initialState={{
+          columns: {
+            columnVisibilityModel: {
+              destTypeId: false,
+            },
+          },
+        }}
+        disableRowSelectionOnClick
+        disableColumnMenu
+        disableColumnSelector
+        disableDensitySelector
+        getRowId={(row) => row.id}
+        density={'comfortable'}
+        pagination
+        slots={{
+          footer: () => <CustomFooter />,
+          toolbar: CustomToolbar as GridSlots['toolbar'],
+        }}
+      />
+      <></>
+    </div>
+  )
+}
+
+export default TestReportTable
---
+++ src/pages/testreport/index.tsx
@@ -0,0 +1,90 @@
+import { Container } from '@mui/material'
+import { getServerSession } from 'next-auth'
+import * as React from 'react'
+import destination from '../../lib/queries/fetch/destination'
+import { authOptions } from '../api/auth/[...nextauth]'
+import connectionTest from '../../lib/connectiontests'
+import cookie from 'cookie'
+import { InferGetServerSidePropsType } from 'next'
+import ErrorBoundary from '../../components/ErrorBoundary'
+import TestReportTable from '../../components/TestReport'
+
+const TestReport = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+  return (
+    <Container maxWidth="xl" title="Test Report">
+      <ErrorBoundary>
+        <TestReportTable
+          destinations={props.destinations}
+          connectionTestResults={props.connectionTestResults}
+          destinationDetails={props.destinationDetails}
+        />
+      </ErrorBoundary>
+    </Container>
+  )
+}
+
+export default TestReport
+export async function getServerSideProps(context) {
+  const { req, res } = context
+
+  const session = await getServerSession(req, res, authOptions)
+  const cookies = cookie.parse(req.headers.cookie || '')
+  const destArray = cookies.destination ? JSON.parse(cookies.destination) : []
+  let destinations = []
+  destinations = destArray.map((item) => {
+    const match = item.match(/([a-zA-Z]+)(\d+)/)
+    return {
+      destId: match ? match[1] : '',
+      destTypeId: match ? parseInt(match[2], 10) : null,
+    }
+  })
+  const results = await Promise.all(
+    destinations.map(async (dest) => {
+      const destinationToTest = await destination(dest.destId, dest.destTypeId)
+      const testResult = await connectionTest(
+        destinationToTest,
+        session.user.email
+      )
+      return {
+        type: destinationToTest.destination_type.type || 'N/A',
+        destId: destinationToTest.dest_id || 'N/A',
+        jurisdiction: destinationToTest.jurisdiction.description || 'N/A',
+        testResults: testResult.connectionTestResult?.testResults || [],
+      }
+    })
+  )
+
+  const connectionTestResults = results.map((result) => {
+    const getTestStatus = (testName) => {
+      const test = result.testResults.find((t) => t.name === testName)
+      return test ? test.status : 'N/A'
+    }
+
+    return {
+      destId: result.destId,
+      destType: result.type,
+      dns: getTestStatus('DNS Lookup Test'),
+      tcp: getTestStatus('TCP Connectivity Test'),
+      tls: getTestStatus('TLS Version Test'),
+      cipher: getTestStatus('Cipher Suites Appropriate'),
+      connectivity: getTestStatus('Connectivity Test'),
+      wsdl: getTestStatus('WSDL Test'),
+      hl7: getTestStatus('HL7 Query Test'),
+    }
+  })
+  const destinationDetails = results.map((result) => ({
+    type: result.type,
+    jurisdiction: result.jurisdiction,
+    destId: result.destId,
+  }))
+
+  return {
+    props: {
+      destinations,
+      connectionTestResults,
+      destinationDetails,
+    },
+  }
+}
