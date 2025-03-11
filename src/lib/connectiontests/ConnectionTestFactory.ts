00f30c6f2eedd93c196328375086bedad7af0394
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-03-11 09:07:12 -0400

    feat: add properties in test factory

--- src/lib/connectiontests/ConnectionTestFactory.ts
+++ src/lib/connectiontests/ConnectionTestFactory.ts
@@ -11,22 +11,24 @@ import QBP from './impl/QBP'
 export default class ConnectionTestFactory {
   static getConnectionTest(
     testName: string,
-    connectionTestRequest: ConnectionTestRequest
+    connectionTestRequest: ConnectionTestRequest,
+    jurisdictionUrl: string,
+    hostname: string
   ): Testable {
     if (testName === 'dns') {
-      return new DNS(connectionTestRequest)
+      return new DNS(connectionTestRequest, jurisdictionUrl)
     }
     if (testName === 'tcp') {
-      return new TCP(connectionTestRequest)
+      return new TCP(connectionTestRequest, jurisdictionUrl)
     }
     if (testName === 'tls') {
-      return new TLS(connectionTestRequest)
+      return new TLS(connectionTestRequest, jurisdictionUrl)
     }
     if (testName === 'cipher') {
       return new CIPHER(connectionTestRequest)
     }
     if (testName === 'wsdl') {
-      return new WSDL(connectionTestRequest)
+      return new WSDL(connectionTestRequest, hostname)
     }
     if (testName === 'connectivity') {
       return new CONNECTIVITY(connectionTestRequest)
