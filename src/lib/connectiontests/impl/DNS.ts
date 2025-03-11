b3e452ac0a348aba2e258ec4e84db0b1cbc274ab
Matthew Stankiewicz <stankm@pointclickcare.com>
2025-03-11 09:12:23 -0400

    feat: change title name w/ property and constructor

--- src/lib/connectiontests/impl/DNS.ts
+++ src/lib/connectiontests/impl/DNS.ts
@@ -3,12 +3,18 @@ import { ConnectionTestResult } from '../types/ConnectionTestResult'
 import { TestStatus } from '../TestStatus'
 import { TestResponseMessages } from '../TestResponseMessages'
 
-const TEST_NAME = 'DNS Lookup Test'
-
 export default class DNS extends ConnectionTest {
+  jurisdictionUrl: string
+
+  constructor(connectionTestRequest, jurisdictionUrl: string) {
+    super(connectionTestRequest)
+    this.jurisdictionUrl = jurisdictionUrl
+  }
+
   run = (): Promise<ConnectionTestResult[]> => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const dns = require('dns')
+    const TEST_NAME = `Verify DNS entry for ${this.jurisdictionUrl}`
     const dnsConnectionTestResult: ConnectionTestResult = {
       name: TEST_NAME,
       order: this.connectionTestRequest.order,
