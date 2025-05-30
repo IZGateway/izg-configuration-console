bbb0ae88fe1b2416d77ae5df07da8c50e32e55ca
Palak Patel <Papatel@ainq.com>
2025-05-30 11:34:26 -0400

    Adding logging for HL7 message

--- src/lib/connectiontests/impl/QBP.ts
+++ src/lib/connectiontests/impl/QBP.ts
@@ -123,6 +123,8 @@ RCP|I|10^RD&amp;Records&amp;HL70126`
       return requestBody
     }
     logger.debug('HL7 message ' + setRequestBody(destinationVersion))
+    logger.debug('Destination ' + JSON.stringify(destination))
+    logger.debug('Password ' + password)
     const httpsAgentOptions = {
       cert: fs.readFileSync(
         path.resolve(this.connectionTestRequest.certPath),
