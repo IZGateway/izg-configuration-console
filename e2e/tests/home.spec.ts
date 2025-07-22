f245468ff7c64e2e9dc20b6e2fd2979dd077c4bd
Palak Patel <57638347+palakpatel311@users.noreply.github.com>
2025-07-22 10:47:55 -0400

    IGDD-2081: Add playwright e2e tests in CICD pipeline (#231)

--- e2e/tests/home.spec.ts
+++ e2e/tests/home.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
-import { loginToOkta } from './helpers/oktaLogin';
+import { loginToOkta } from '../helpers/oktaLogin';
 
 test('homepage has correct username of logged in user', async ({ page }) => {
   await loginToOkta(page, process.env.OKTA_USERNAME, process.env.OKTA_PASSWORD);
