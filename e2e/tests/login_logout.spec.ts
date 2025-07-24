4a7af193b031eb879e07429d111145737a610110
Austin Moody <austinmoody@protonmail.com>
2025-07-24 11:57:03 -0400

    Add test to verify when unauthorized user tries to login.

--- e2e/tests/login_logout.spec.ts
+++ e2e/tests/login_logout.spec.ts
@@ -34,6 +34,31 @@ test('Verify Connections Table, for Admin, for all row count options', async ({ 
   await page.locator('#logout').click();
 });
 
+// Test 12 - a - i
+// ON HOLD - need non-admin user setup in Okta
+
+// Test 12 - a- iii
+test('Verify unauthorized user cannot login', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'load', timeout: 10000 })
+
+  await page.getByText('Sign in with Okta').click()
+  await page.locator('input[name="identifier"]').fill('fakeuser@izgateway.org')
+  await page.locator('[type="submit"]').click()
+
+  const body = page.locator('body')
+  const selectFactorExists = await body
+    .locator('.button.select-factor.link-button')
+    .count()
+  if (selectFactorExists > 0) {
+    await body.locator('.button.select-factor.link-button').click()
+  }
+  await page.locator('input[name="credentials.passcode"]').fill('THIS IS A BOGUS PASSWORD')
+  await page.locator('[type="submit"]').click()
+
+  await expect(page.locator('.okta-form-infobox-error')).toContainText('Unable to sign in');
+
+})
+
 // Test 12 - iv
 test('Verify Login then Logout', async ({ page }) => {
   await loginToOkta(page, process.env.OKTA_USERNAME, process.env.OKTA_PASSWORD);
