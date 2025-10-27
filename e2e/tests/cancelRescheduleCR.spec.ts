59a529857cf74108dd5577383752aaa31e2d5af6
Palak Patel <papatel@ainq.com>
2025-10-27 12:44:04 -0400

    made helper function

--- e2e/tests/cancelRescheduleCR.spec.ts
+++ e2e/tests/cancelRescheduleCR.spec.ts
@@ -1,6 +1,8 @@
 import { Page, expect, test } from '@playwright/test'
 import { loginToOkta } from '../helpers/oktaLogin'
 import { logout } from '../helpers/logout'
+import { filterByDestinationId } from '../helpers/filterByDestinationId'
+import { createChangeRequest } from '../helpers/createChangeRequest'
 
 let context
 let page: Page
@@ -19,102 +21,12 @@ test.afterAll(async () => {
   await context.close()
 })
 
-const newValues = {
-  uri: 'https://example.com',
-  username: 'UsernameNEW',
-  password: 'NEWpassword123!!',
-  facilityId: 'xyz',
-  MSH3: 'MSH3',
-  MSH4: 'MSH4',
-  MSH5: 'MSH5',
-  MSH6: 'MSH6',
-  MSH22: 'MSH22',
-  MSH11: 'P',
-  RXA11: 'RXA11',
-}
 const destId = 'dev2011'
-async function filterByDestinationId(page: Page, destId: string) {
-  await page.locator('button[aria-label="Show filters"]').click()
-  await page.locator('[role="combobox"]:has-text("contains")').click()
-  await page.getByRole('option', { name: 'equals' }).click()
-  await page.getByRole('textbox', { name: /value/i }).fill(destId)
-  await page.getByText('My Connections').click() // Click anywhere to close filter pop up
-}
-
-async function createChangeRequest() {
-  const nextButton = page.locator('#next')
-  const scheduleButton = page.locator('#schedule')
-  const alert = page.locator('[class*="MuiAlert-message"]')
-
-  const editButton = page.locator('button[aria-label="edit"]')
-
-  // filter table by dest id
-  await filterByDestinationId(page, destId)
-  const hasEditButton = (await editButton.count()) > 0
-
-  if (hasEditButton) {
-    // if cr is not created
-    await editButton.click()
-    if (page.getByTestId('agree-button').isVisible()) {
-      await page.getByTestId('agree-button').click()
-      await page.locator('#accept').click()
-    }
-
-    await nextButton.click()
-
-    // close save draft alert if there
-    if (await alert.isVisible()) {
-      const closeButton = page.locator('[title="Close"]')
-      await closeButton.click()
-    }
-
-    //start editing
-    await page.locator('#destUri').fill(newValues.uri)
-    await page.locator('#username').fill(newValues.username)
-    await page.getByRole('button', { name: 'CHANGE PASSWORD' }).click()
-    await page.locator('[name="newPassword"]').fill(newValues.password)
-    await page.locator('[name="confirmPassword"]').fill(newValues.password)
-    await page.locator('[name="facilityId"]').fill(newValues.facilityId)
-    await page.locator('[name="MSH3"]').fill(newValues.MSH3)
-    await page.locator('[name="MSH4"]').fill(newValues.MSH4)
-    await page.locator('[name="MSH5"]').fill(newValues.MSH5)
-    await page.locator('[name="MSH6"]').fill(newValues.MSH6)
-    await page.locator('[name="MSH22"]').fill(newValues.MSH22)
-    await page.locator('[name="MSH11"]').fill(newValues.MSH11)
-    await page.locator('[name="RXA11"]').fill(newValues.RXA11)
-    await nextButton.click()
-
-    // check submitting values
-    const rows = page.locator('table tbody tr')
-    const submittingValues: string[] = []
-    const rowCount = await rows.count()
-    for (let i = 0; i < rowCount; i++) {
-      const submittingCell = rows.nth(i).locator('td').nth(1)
-      const value = await submittingCell.textContent()
-      submittingValues.push(value?.trim() || '')
-    }
-    Object.values(newValues).forEach((value) => {
-      expect(submittingValues).toContain(value)
-    })
-
-    await nextButton.click()
-
-    //schedule
-    await page.locator('[value="As Soon As Possible"]').click()
-    await scheduleButton.click()
-
-    await page.waitForURL('/manageconnections')
-    await expect(alert).toBeVisible()
-    expect(await alert.textContent()).toContain(
-      'Change request is created successfully'
-    )
-  }
-}
 
 test('User should be able to reschedule submitted CR', async () => {
   const changeRequestButton = page.locator('button[aria-label="changerequest"]')
 
-  await createChangeRequest()
+  await createChangeRequest(page, destId)
   if ((await page.locator('button[aria-label="edit"]').count()) > 0) {
     await filterByDestinationId(page, destId)
   }
@@ -127,7 +39,7 @@ test('User should be able to reschedule submitted CR', async () => {
 
 test('User should be able to cancel submitted CR', async () => {
   const changeRequestButton = page.locator('button[aria-label="changerequest"]')
-  await createChangeRequest()
+  await createChangeRequest(page, destId)
   if ((await page.locator('button[aria-label="edit"]').count()) > 0) {
     await filterByDestinationId(page, destId)
   }
